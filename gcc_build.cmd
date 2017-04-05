caccf78cefea1a213940065e36ec0c2bfff51c39
Incubator85653 <2bemperor47@gmail.com>
2017-04-05 15:08:49 +0800

    Update gcc compile script
    - Pause.

--- gcc_build.cmd
+++ gcc_build.cmd
@@ -10,4 +10,5 @@ windres gcc_tensokukan_network_launcher.rc -O coff -o gcc_tensokukan_network_lau
 g++ -std=c++11 -D_UNICODE -DUNICODE -o gcc_Build\tensokukan_network_launcher.exe *.cpp *.res -lShlwapi
 @echo.
 
+@pause
 @exit
